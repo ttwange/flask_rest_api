1ea317fc989cbb56aa979d00c2b481f8b4db14c6
ttwange <ttwange2@gmail.com>
2023-10-11 09:56:49 +0300

    Working on flask rest api

--- flask/app.py
+++ flask/app.py
@@ -1,16 +1,3 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return 'Flask world'
-
-
-@app.route('/<name>')
-def print_name(name):
-  return f'Hello, {name}'
-
-
-if __name__ == '__main__':
-  app.run()
+app =Flask(__name__)
