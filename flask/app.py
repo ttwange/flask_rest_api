66b92df77b84a1e526c930801e779f45ff0536de
ttwange <ttwange2@gmail.com>
2023-10-13 12:15:10 +0300

    Working on flask rest api

--- flask/app.py
+++ flask/app.py
@@ -124,56 +124,55 @@ books_list = [
 "title": "Season of Migration to the North"
 }
 ]
-
-@app.route('/books',methods=['GET','POST'])
+@app.route('/books', methods=['GET', 'POST'])
 def books():
-  if request.method == 'GET':
-    if len(books_list) > 0:
-      return jsonify(books_list)
-    else:
-      'Nothing Found', 404
+    if request.method == 'GET':
+        if len(books_list) > 0:
+            return jsonify(books_list)
+        else:
+            return 'Nothing Found', 404
 
-  if request.method == 'POST':
-    new_author = request.form['author']
-    new_lang = request.form['language']
-    new_title=request.form['title']
-    iD = books_list[-1]['id']+1
+    if request.method == 'POST':
+        new_author = request.form['author']
+        new_lang = request.form['language']
+        new_title = request.form['title']
+        iD = books_list[-1]['id'] + 1
 
-    new_obj = {
-      'id': iD,
-      'author': new_author,
-      'language': new_lang,
-      'title':new_title
-    }
-    books_list.append(new_obj)
-    return jsonify(books_list), 201
-  
-@app.route('/book/<int:id>', methods=['GET','PUT', 'DELETE'])
+        new_obj = {
+            'id': iD,
+            'author': new_author,
+            'language': new_lang,
+            'title': new_title
+        }
+        books_list.append(new_obj)
+        return jsonify(books_list), 201
+
+@app.route('/book/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def single_book(id):
-  if request.method == 'GET':
-    for book in books_list:
-      if book['id'] ==id:
-        return jsonify(book)
-      pass
-  if request.method == 'GET':
-    for book in books_list:
-      if book['id'] ==id:
-        book['author'] = request.form['author']
-        book['language'] = request.form['language']
-        book['title'] = request.form['title']
-        updated_book = {
-          'id':id,
-          'author': book['author'],
-          'language': book['language'],
-          'title' : book ['title']
-          }
-        return jsonify(updated_book)
-  if request.method == 'DELETE':
-    for index, book in enumerate(books_list):
-      if books['id'] == id:
-        books_list.pop(index)
-        return jsonify(books_list)
+    if request.method == 'GET':
+        for book in books_list:
+            if book['id'] == id:
+                return jsonify(book)
 
+    if request.method == 'PUT':
+        for book in books_list:
+            if book['id'] == id:
+                book['author'] = request.form['author']
+                book['language'] = request.form['language']
+                book['title'] = request.form['title']
+                updated_book = {
+                    'id': id,
+                    'author': book['author'],
+                    'language': book['language'],
+                    'title': book['title']
+                }
+                return jsonify(updated_book)
+
+    if request.method == 'DELETE':
+        for index, book in enumerate(books_list):
+            if book['id'] == id:
+                books_list.pop(index)
+                return jsonify(books_list)
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
