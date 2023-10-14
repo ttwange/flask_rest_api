9255779719f600b19456248b6b7d1afde0a9fd33
ttwange <ttwange2@gmail.com>
2023-10-14 12:21:22 +0300

    Added the connection to sqlite3 as a function

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, jsonify
+import json
+import sqlite3
 
 app = Flask(__name__)
+
+def db_connection():
+    conn = None:
+    try:
+        conn = sqlite3.connect('books.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
+                               
 
 books_list = [
 {
